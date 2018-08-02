829009362357ce13b2fd9949d0f96b802367425b
Cristiano Diniz Pinto <cristianodp@gmail.com>
2018-08-02 02:12:13 -0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-
+const port = process.env.PORT || 5000
 
 const server = require("./config/server");
 const routes = require('./config/routes');
 require("./config/database");
 routes(server);
 
-server.listen(3000, () => {
-    console.log("running on port 3000");
+server.listen(port, () => {
+    console.log("running on port "+port);
 });
