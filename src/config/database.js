685c717bcf51b04930d85a9a46cdedc84bbeb2e8
Cristiano Diniz Pinto <cristianodp@gmail.com>
2018-08-02 01:56:39 -0300

    initial commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 //module.exports =  mongoose.connect('mongodb://db/dolphinIt')
-module.exports =  mongoose.connect("mongodb://db:27017/dolphinIT", { useNewUrlParser: true });
+
+const url = process.env.MONGOLAB_URI? process.env.MONGOLAB_URI : "mongodb://db:27017/dolphinIT"
+
+module.exports =  mongoose.connect(url, { useNewUrlParser: true });
+
 /*
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' é menor que o limite mínimo de '{MIN}'."
